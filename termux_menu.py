9a1f8e0bb51f07bdaa3b950d5705998823d9ba24
Degaultnameu <152231256+Degaultnameu@users.noreply.github.com>
2025-04-25 23:42:26 -0300

    Update termux_menu.py

--- termux_menu.py
+++ termux_menu.py
@@ -102,7 +102,7 @@ xml_layout = """
     android:layout_width="match_parent"
     android:layout_height="match_parent">
 
-    <TextView android:id="@+id/title" android:layout_width="wrap_content" android:layout_height="wrap_content" android:text="TERMUX PRO" />
+    <TextView android:id="@+id/title" android:layout_width="wrap_content" android:layout_height="wrap_content" android:text="TERMURO" />
     <TextView android:id="@+id/loading" android:layout_width="wrap_content" android:layout_height="wrap_content" android:text="" />
     <Button android:id="@+id/terminal" android:layout_width="wrap_content" android:layout_height="wrap_content" android:text="Terminal" />
     <Button android:id="@+id/update" android:layout_width="wrap_content" android:layout_height="wrap_content" android:text="Atualizar Pacotes" />
